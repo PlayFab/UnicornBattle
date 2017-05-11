fdeaebaa0194d7ad44c9b060121318c6fa4c3b9e
pgilmorepf <paul@playfab.com>
2017-05-11 15:11:53 -0700

    Trivial Cleanup

--- UnicornBattle/Assets/Scripts/Misc/CanvasController.cs
+++ UnicornBattle/Assets/Scripts/Misc/CanvasController.cs
@@ -1,11 +1,9 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
-using System.Collections;
+using UnityEngine;
 
-public class CanvasController : Singleton<CanvasController> {
-	protected CanvasController () {} // guarantee this will be always a singleton only - can't use the constructor!
-	
-	public Transform settingsMenu;
-	public Transform modalAlert;
+public class CanvasController : Singleton<CanvasController>
+{
+    protected CanvasController() { } // guarantee this will be always a singleton only - can't use the constructor!
+
+    public Transform settingsMenu;
+    public Transform modalAlert;
 }
