31a139f0051b23ce88b59e3d3fd1912147cd3ab0
Andy McCalib <andmcc@microsoft.com>
2018-10-12 16:29:58 -0700

    Updating to latest PlayFab Extension and SDK

--- UnicornBattle/Assets/PlayFabEditorExtensions/Editor/Scripts/Utils/PlayFabEditorVersion.cs
+++ UnicornBattle/Assets/PlayFabEditorExtensions/Editor/Scripts/Utils/PlayFabEditorVersion.cs
@@ -1,1 +1,1 @@
-namespace PlayFab.PfEditor { public static partial class PlayFabEditorHelper { public static string EDEX_VERSION = "2.52.180924"; } }
+namespace PlayFab.PfEditor { public static partial class PlayFabEditorHelper { public static string EDEX_VERSION = "2.53.181001"; } }
